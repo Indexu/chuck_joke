e39179c4add51c0fe9091a3c231f1f918c9c199f
Hilmar Tryggvason <hilmart@gmail.com>
2016-10-28 15:40:56 +0000

    Added servers

--- src/selenium/java/is/ru/arnlaugsson/chuck_joke/SeleniumTestWrapper.java
+++ src/selenium/java/is/ru/arnlaugsson/chuck_joke/SeleniumTestWrapper.java
@@ -5,6 +5,10 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public abstract class SeleniumTestWrapper {
     //static ChromeDriver driver;
@@ -18,15 +22,54 @@ public abstract class SeleniumTestWrapper {
         driver = new PhantomJSDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        port = System.getenv("PORT");
-        if (port == null) {
-            port = "4567";
-        }
-        baseUrl = "https://staging-chuckjoke.herokuapp.com";
+        setBaseURL();
+
+		if(baseUrl.contains("localhost"))
+		{
+			port = System.getenv("PORT");
+	        if (port == null) {
+	            port = "4567";
+	        }
+
+			baseUrl += port;
+		}
     }
 
     @AfterClass
     public static void closeBrowser(){
         driver.quit();
+    }
+
+    private static void setBaseURL()
+    {
+		String serverFilePath = System.getProperty("user.dir") + "/servers/server.txt";
+        try
+		{
+			BufferedReader reader;
+			reader = new BufferedReader(new FileReader(serverFilePath));
+			try
+			{
+				baseUrl = reader.readLine();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+			finally
+			{
+				try
+				{
+					reader.close();
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
     }
 }
